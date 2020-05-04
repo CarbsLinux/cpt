6542d434d279e092ef54144a8d887c6e26f05482
Cem Keylan <cem@ckyln.com>
2020-05-04 13:24:51 +0300

    kiss-readlink: add 'readlink -f' replacement

---
+++ bin/kiss-readlink.c
@@ -0,0 +1,25 @@
+// kiss-readlink --- a utility replacement for readlink
+// See LICENSE for copyright information
+
+// This is basically a 'readlink -f' command.
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+
+  char buf[512];
+
+  if (argc != 2) {
+    printf("usage: %s FILE\n", argv[0]);
+    return(1);
+  }
+
+  if (!realpath(argv[1], buf)) {
+    perror("realpath:");
+    return(1);
+  }
+
+  // fputs(buf,stdout);
+  printf("%s\n", buf);
+  return(0);
+}
