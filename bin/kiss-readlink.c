b098110b5c878cd72dee80d9997f6d4376875794
Cem Keylan <cem@ckyln.com>
2020-05-15 17:05:15 +0300

    kiss-readlink: canonicalize directories if files do not exist.
    kiss-readlink will no longer fail if the targeted symlink doesn't
    exist. This is similar to how 'readlink -f' works. The previous
    behaviour was similar to 'readlink -e' from coreutils, i.e it wanted
    all targets to exist.

--- bin/kiss-readlink.c
+++ bin/kiss-readlink.c
@@ -4,11 +4,15 @@
 // This is basically a 'readlink -f' command.
 #include <stdio.h>
 #include <stdlib.h>
+#include <libgen.h>
 #include <string.h>
 
 int main(int argc, char *argv[]) {
 
   char buf[512];
+  char bname[512];
+  strcpy(bname, "/");
+  strcat(bname, (basename(argv[1])));
 
   if (argc != 2 || strcmp(argv[1], "--help") == 0) {
     printf("usage: %s <file>\n", argv[0]);
@@ -16,8 +20,12 @@ int main(int argc, char *argv[]) {
   }
 
   if (!realpath(argv[1], buf)) {
+
+    if (!realpath(dirname(argv[1]), buf)) {
     perror("realpath");
     return(1);
+    }
+    strcat(buf, bname);
   }
 
   printf("%s\n", buf);
