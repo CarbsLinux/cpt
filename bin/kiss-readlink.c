afd4c4072ae0a46cd39edf78173a9ce884d729c0
Cem Keylan <cem@ckyln.com>
2020-06-08 09:42:02 +0300

    bin: change usage

--- bin/kiss-readlink.c
+++ bin/kiss-readlink.c
@@ -15,7 +15,7 @@ int main(int argc, char *argv[]) {
   strcat(bname, (basename(argv[1])));
 
   if (argc != 2 || strcmp(argv[1], "--help") == 0) {
-    printf("usage: %s <file>\n", argv[0]);
+    printf("usage: %s [file]\n", argv[0]);
     return(1);
   }
 
