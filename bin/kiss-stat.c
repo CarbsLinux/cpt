22d2433a2f29379bbe6a4827dfbf438bfacb07a4
merakor <cem@ckyln.com>
2020-06-08 06:55:30 +0000

    bin: C89 standard compatibility fixes
    FossilOrigin-Name: 0f850280cc94a62782bf1fc83fa3b719678bf529f7101387c017d265fdd6a2f1

--- bin/kiss-stat.c
+++ bin/kiss-stat.c
@@ -1,7 +1,7 @@
-// kiss-stat --- a utility for getting the user name of file owner
-// See LICENSE for copyright information
-
-/* The reason this simple tool exists is because 'stat' is not
+/* kiss-stat --- a utility for getting the user name of file owner
+ * See LICENSE for copyright information
+ *
+ * The reason this simple tool exists is because 'stat' is not
  * portable and ls is not exactly stable enough for scripting.
  * This program is for outputting the owner name, and that's it.
  */
@@ -11,28 +11,30 @@
 #include <stdio.h>
 #include <string.h>
 
-int main (int argc, char *argv[]) {
-  struct stat sb;
+struct passwd *pw;
+struct stat    sb;
 
-  // Exit if no or multiple arguments are given.
+int main (int argc, char *argv[]) {
+
+  /* Exit if no or multiple arguments are given. */
   if (argc != 2 || strcmp(argv[1], "--help") == 0) {
     fprintf(stderr, "Usage: %s [pathname]\n", argv[0]);
     return(1);
   }
 
-  // Exit if file stat cannot be obtained.
+  /* Exit if file stat cannot be obtained. */
   if (lstat(argv[1], &sb) == -1) {
     perror(argv[0]);
     return(1);
   }
 
-  // Exit if name of the owner cannot be retrieved.
+  /* Exit if name of the owner cannot be retrieved. */
   if (!getpwuid(sb.st_uid)) {
     return(1);
   }
 
-  // Print the user name of file owner.
-  struct passwd *pw = getpwuid(sb.st_uid);
+  /* Print the user name of file owner. */
+  pw = getpwuid(sb.st_uid);
   printf("%s\n", pw->pw_name);
   return(0);
 }  
