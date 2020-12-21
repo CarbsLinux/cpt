cadb3dc73197e651ce9782cd1529905c183a2700
merakor <cem@ckyln.com>
2020-12-21 11:34:22 +0000

    dir-local: fix excluded warning list
    FossilOrigin-Name: aaaa279be11e88bfa2745b442a2543ef176cd79d9d5537f25e6f93928bd61ae4

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,4 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((sh-mode . ((flycheck-shellcheck-excluded-warnings . '(2119)))))
+((sh-mode . ((flycheck-shellcheck-excluded-warnings . '("2119")))))
